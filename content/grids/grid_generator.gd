cc6f997d3cc30294962b5aded636d1e5f00a7ff7
WoolyDrool <nikolai.mcneely15@gmail.com>
2026-02-11 16:14:14 -0800

    Cleaned up script

--- content/grids/grid_generator.gd
+++ content/grids/grid_generator.gd
@@ -5,37 +5,32 @@ extends Area3D
 var tile_size = 4
 @export var tile_scene : PackedScene
 
+@onready var grid_volume : CollisionShape3D = $GridVolume
+var grid_volume_size : Vector3
 
 @export_tool_button("Generate Grid") 
 var generate_grid_action : Callable = try_generate_grid
+var requested_size_x : int = 8
+var requested_size_z : int = 8
+
+var cur_gen_index_x : int = 0
+var cur_gen_index_z : int = 0
 
 @export_tool_button("Reset Grid") 
 var reset_grid_action : Callable = reset_grid
 
-@onready var grid_volume : CollisionShape3D = $GridVolume
-var grid_volume_size : Vector3
-var requested_size_x : int = 8
-var requested_size_z : int = 8
-
-var cur_gen_index_z : int = 0
-var cur_gen_index_x : int = 0
-
-
 #TODO: Define the width and length of the volume to an array
 #TODO: Generate tiles recursively until finished
 
-func reset_grid():
-	grid_volume.shape.size = Vector3(tile_size, tile_size, tile_size)
-	grid_volume.shape.position = position
-	for child in get_children():
-		child.queue_free()
-
+#region Helpers
 func is_req_size_valid() -> bool:
-	var grid_volume_size : Vector3 = grid_volume.shape.size
+	grid_volume_size = grid_volume.shape.size
 	
-	requested_size_x = grid_volume_size.x
-	requested_size_z = grid_volume_size.y
+	requested_size_x = grid_volume_size.x as int
+	requested_size_z = grid_volume_size.y as int
 	
+	# This determines wether or not the requested size divides evenly into tile_size 
+	# tile_size should always be 4 but i made it a variable just in case things change later in development
 	if requested_size_x % tile_size == 0 && requested_size_z % tile_size == 0:
 		return true
 	else:
@@ -47,6 +42,7 @@ func try_generate_grid():
 		generate_grid()
 	else:
 		push_error("GridGenerator: Grid generation failed!")
+#endregion
 
 func generate_grid():
 	cur_gen_index_x = 0
@@ -69,14 +65,8 @@ func generate_next_x_row():
 		cur_gen_index_x += tile_size 
 		generate_next_x_row()
 	else:
-		print("GridGenerator: Finished current X row. Moving to next Z row")
+		print("GridGenerator: Finished current X row at ",cur_gen_index_x, ", Moving to next Z row")
 		generate_next_z_row()
-
-func generate_next_z_row():		
-	if !cur_z_grid_done():
-		cur_gen_index_x = 0
-		cur_gen_index_z += tile_size
-		generate_next_x_row()
 
 func cur_x_grid_done() -> bool:
 	if cur_gen_index_x == requested_size_x:
@@ -84,12 +74,22 @@ func cur_x_grid_done() -> bool:
 	else:
 		return false
 
+func generate_next_z_row():		
+	if !cur_z_grid_done():
+		cur_gen_index_x = 0
+		cur_gen_index_z += tile_size
+		generate_next_x_row()
+
 func cur_z_grid_done() -> bool:
 	if cur_gen_index_z == requested_size_z:
 		return true
 	else:
 		return false
 
-
+func reset_grid():
+	grid_volume.shape.size = Vector3(tile_size, tile_size, tile_size)
+	grid_volume.shape.position = position
+	for child in get_children():
+		child.queue_free()
 	
 	
