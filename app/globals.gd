6daf37a1f271cbeedf2cbefc331a72a2f78c3b7b
WoolyDrool <nikolai.mcneely15@gmail.com>
2026-02-23 13:37:21 -0800

    Added map relevant signals

--- app/globals.gd
+++ app/globals.gd
@@ -5,3 +5,7 @@ var current_tick : int = 0
 @warning_ignore("unused_signal")
 signal time_advance_tick
 #endregion
+
+#region Maps/Map Transitions
+signal map_transition(new_map : Map, trans_point : MapTransitionPoint)
+#endregion
