91c45df795b794a4ba96ae9b32d1ca63b852308b
WoolyDrool <nikolai.mcneely15@gmail.com>
2026-02-26 14:41:15 -0800

    Further improvements

--- systems/interaction/interaction_manager.gd
+++ systems/interaction/interaction_manager.gd
@@ -11,18 +11,21 @@ var can_scan : bool = true
 var collision_count : int
 var can_cycle : bool = false
 var wanted_position : Vector3
-var lerp_speed : float = 8
+var lerp_speed : float = 16
+var sprite_default_pos : Vector3
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	Globals.time_advance_tick.connect(scan_for_new_interactables)
+	sprite_default_pos = selection_sprite.global_position
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("scan_for_interacts") and can_scan:
-		scan_for_new_interactables()
+	#if Input.is_action_just_pressed("scan_for_interacts") and can_scan:
+		#scan_for_new_interactables()
 	
 	selector_cycle_next() 
+
 
 func _physics_process(delta: float) -> void:
 	selection_sprite.global_position = lerp(selection_sprite.global_position, wanted_position, lerp_speed * delta)
@@ -38,7 +41,7 @@ func scan_for_new_interactables() -> void:
 		var collider = shapecast.get_collider(i)
 		print(collider.entity_name)
 		in_range_interactables.append(collider)
-			
+	
 	if collision_count == shapecast.get_collision_count():
 		cooldown_timer.start()
 	
@@ -58,9 +61,16 @@ func selector_cycle_next():
 	elif current_interaction_index < 0:
 		current_interaction_index = 0
 	
+	# Lerp
 	if collision_count > 0:
+		if !selection_sprite.visible:
+			selection_sprite.visible = true
 		wanted_position = in_range_interactables.get(current_interaction_index).global_position
 		#selection_sprite.global_position = in_range_interactables.get(current_interaction_index).global_position
+	else:
+		if selection_sprite.visible:
+			selection_sprite.visible = false
+		wanted_position = sprite_default_pos
 	
 func _on_interaction_cooldown_timer_timeout() -> void:
 	can_scan = true
