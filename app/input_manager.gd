54690c4f6f4788cea57604ed8e897b50ac7dc0a6
WoolyDrool <nikolai.mcneely15@gmail.com>
2026-05-09 16:46:10 -0700

    Defined base input mappings and actions

---
+++ app/input_manager.gd
@@ -0,0 +1,54 @@
+class_name InputManager
+extends Node
+
+@export_category("Gamepad")
+@export var switch_to_gamepad : GUIDEAction
+@export var global_gamepad : GUIDEMappingContext
+@export var move_mode_gamepad : GUIDEMappingContext
+@export var ui_mode_gamepad : GUIDEMappingContext
+
+@export_category("KBM")
+@export var switch_to_kbm : GUIDEAction
+@export var global_kbm : GUIDEMappingContext
+@export var move_mode_kbm : GUIDEMappingContext
+@export var ui_mode_kbm : GUIDEMappingContext
+
+enum GAME_MODE {MOVE, UI}
+var game_mode : GAME_MODE = GAME_MODE.MOVE
+
+enum INPUT_MODE {KBM, GAMEPAD}
+var input_mode : INPUT_MODE = INPUT_MODE.GAMEPAD
+
+func _ready() -> void:
+	switch_to_gamepad.triggered.connect(set_input_mode.bind(INPUT_MODE.GAMEPAD))
+	switch_to_kbm.triggered.connect(set_input_mode.bind(INPUT_MODE.KBM))
+	SignalBus.change_input_mode_to_gameplay.connect(set_gamemode.bind(GAME_MODE.MOVE))
+	SignalBus.change_input_mode_to_ui.connect(set_gamemode.bind(GAME_MODE.UI))
+	update_input()
+	
+func update_input():
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	match input_mode:
+		INPUT_MODE.KBM:
+			GUIDE.enable_mapping_context(global_kbm, true)
+			match game_mode:
+				GAME_MODE.MOVE:
+					GUIDE.enable_mapping_context(move_mode_kbm)
+				GAME_MODE.UI:
+					Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+					GUIDE.enable_mapping_context(ui_mode_kbm)
+		INPUT_MODE.GAMEPAD:
+			GUIDE.enable_mapping_context(global_gamepad, true)
+			match game_mode:
+				GAME_MODE.MOVE:
+					GUIDE.enable_mapping_context(move_mode_gamepad)
+				GAME_MODE.UI:
+					GUIDE.enable_mapping_context(ui_mode_gamepad)
+	
+func set_gamemode(_game_mode : GAME_MODE):
+	game_mode = _game_mode
+	update_input()
+
+func set_input_mode(_input_mode : INPUT_MODE):
+	input_mode = _input_mode
+	update_input()
