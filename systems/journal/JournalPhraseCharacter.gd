2f93480242628e754c6a48a617426ba80c92796b
WoolyDrool <nikolai.mcneely15@gmail.com>
2026-05-11 10:19:20 -0700

    Removed old attempt at the journal selector system becuase we found a better way

--- systems/journal/JournalPhraseCharacter.gd
+++
@@ -1,7 +0,0 @@
-class_name JournalPhraseCharacter
-extends Resource
-
-@export var character_name : String = "Test Character Name"
-@export var character_text_1 : String = "Test Phrase"
-@export var character_journal_text_1 : String = "Test Journal Phrase"
-@export var character_journal_text_1_completed : bool = false
