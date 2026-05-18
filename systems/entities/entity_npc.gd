7f7e49db8a32b47edf563886df313a865dc04172
WoolyDrool <nikolai.mcneely15@gmail.com>
2026-05-18 16:28:11 -0700

    Created a shell NPC base for vik

--- systems/entities/entity_npc.gd
+++ systems/entities/entity_npc.gd
@@ -2,11 +2,11 @@ class_name NPCEntity
 extends Node
 
 @export var dialogic_timeline : DialogicTimeline
-@export var dialogic_character : DialogicCharacter
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	if !dialogic_timeline:
+		push_error("EntityNPC: No Dialogic Timeline Selected!")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
