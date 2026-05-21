040732257d8796907cb3512a09f352c70c926bca
WoolyDrool <nikolai.mcneely15@gmail.com>
2026-05-21 14:35:34 -0700

    Created custom tile type for stairs

---
+++ systems/grid/tile_stair.gd
@@ -0,0 +1,11 @@
+class_name TileStair
+extends Tile
+
+@export var custom_y_value : float
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	center = get_stair_center()
+		
+func get_stair_center() -> Vector3:
+	return Vector3(global_position.x, custom_y_value, global_position.z)
