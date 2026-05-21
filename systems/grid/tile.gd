f7fd9b4658d0987d28d754b079b5d99b4cfe78df
WoolyDrool <nikolai.mcneely15@gmail.com>
2026-05-21 14:35:12 -0700

    Cleaned up tile script in preparation for making the stair version

--- systems/grid/tile.gd
+++ systems/grid/tile.gd
@@ -1,22 +1,11 @@
 class_name Tile
 extends Area3D
 
-@export var use_custom_center_point : bool = false
 @export var center : Vector3
-
-signal on_tile_enter
-signal on_tile_exit
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	if !use_custom_center_point:
-		center = get_center_point()
-
+	center = get_center_point()
+		
 func get_center_point() -> Vector3:
-	return self.global_position
-
-func enter_tile():
-	pass
-
-func exit_tile():
-	pass
+	return global_position
