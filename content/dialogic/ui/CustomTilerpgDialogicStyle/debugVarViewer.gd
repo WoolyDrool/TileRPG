1cdbf80d83cb2bbd881015af652a87dd5a581124
Viktor Love <theawkwarddreamer@gmail.com>
2026-07-07 10:39:07 -0700

    starting on debug viewer

--- content/dialogic/ui/CustomTilerpgDialogicStyle/debugVarViewer.gd
+++ content/dialogic/ui/CustomTilerpgDialogicStyle/debugVarViewer.gd
@@ -1,14 +1,15 @@
 extends RichTextLabel
 
-@onready var debugHost = get_node("debug")
-# get_node("debugVariables/debug").set_text("testing")
+# I'm only able to set this on startup, but hopefully it's enough for you to build on
+func writeVariables():
+	var qBeastStatus = "Questing Beast Status: " + Dialogic.VAR.questProgress.questingBeast.investigationStatus
+	var sequenceStatus = "Sequence Status: " + Dialogic.VAR.questProgress.sequenceRepair.sequenceStatus
+	text = qBeastStatus + "\n" + sequenceStatus
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
-
-func writeVariables():
-	text = "testing"
+	writeVariables()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	writeVariables()
+	pass
